67c4d0ac4b8784de2c90aadaafa71b2f5aeb9e63
natan.daniel <natan.daniel0@gmail.com>
2019-07-22 21:31:22 +0200

    function

---
+++ functional_interface/src/function/FunctionMain.java
@@ -0,0 +1,17 @@
+package function;
+
+import java.util.function.Function;
+
+public class FunctionMain {
+
+	public static void main(String[] args) {
+		
+		
+		Function<String, Integer> fromTextToInt = (aNumberAsText) -> Integer.valueOf(aNumberAsText);
+		
+		int fortyTwo = fromTextToInt.apply("42");
+		
+		System.out.println(fortyTwo);
+	}
+
+}
