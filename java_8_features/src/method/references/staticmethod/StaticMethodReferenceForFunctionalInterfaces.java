2ebc9da862abd3f6b782480de376fe7c648cbb3f
natan.daniel <natan.daniel0@gmail.com>
2019-08-10 20:15:36 +0200

    static method reference for functional interface implementations

---
+++ java_8_features/src/method/references/staticmethod/StaticMethodReferenceForFunctionalInterfaces.java
@@ -0,0 +1,31 @@
+package method.references.staticmethod;
+
+/**
+ * Java 8's static method reference can be used as an alternative to lambdas to
+ * provide functional interface implementations.
+ */
+public class StaticMethodReferenceForFunctionalInterfaces {
+
+	@FunctionalInterface
+	interface HelloFunctionalInterface {
+		String sayHelloTo(String name);
+
+	}
+
+	public static void main(String[] args) {
+
+		HelloFunctionalInterface helloInFrenchWithLambda = (name) -> "Bonjour " + name;
+
+		HelloFunctionalInterface helloInEnglishWithStaticMethodReference = StaticMethodReferenceForFunctionalInterfaces::sayHello;
+
+		System.out.println(helloInFrenchWithLambda.sayHelloTo("Monde"));
+
+		System.out.println(helloInEnglishWithStaticMethodReference.sayHelloTo("World"));
+
+	}
+
+	public static String sayHello(String name) {
+		return "Hello " + name;
+	}
+
+}
