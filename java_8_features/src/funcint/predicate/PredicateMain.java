ace13785b7be75cf749bd43df4eba7a195be9a30
Natan Daniel <natan.daniel0@gmail.com>
2020-01-12 22:07:50 +0100

    nesting Cat within PredicateMain

--- java_8_features/src/funcint/predicate/PredicateMain.java
+++ java_8_features/src/funcint/predicate/PredicateMain.java
@@ -2,24 +2,43 @@ package funcint.predicate;
 
 import java.util.function.Predicate;
 
-public class PredicateMain {
-	
+class PredicateMain {
+
+	private static class Cat {
+
+		private String name;
+		private float age;
+
+		public Cat(String name, float age) {
+			this.name = name;
+			this.age = age;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public float getAge() {
+			return age;
+		}
+	}
+
 	public static void main(String[] args) {
-				
-		Predicate<Cat> isCatOlderThanOne = (cat) -> (cat.getAge()  >= 1.0f);
-		
-		Cat pythie = new Cat("Pythie", 5);
+
+		Predicate<Cat> isCatOlderThanOne = (cat) -> (cat.getAge() >= 1.0f);
+
+		Cat pythie = new Cat("Pythie", 5.0f);
 		Cat minou = new Cat("Minou", 0.5f);
-		
-		if(isCatOlderThanOne.test(pythie)) {
+
+		if (isCatOlderThanOne.test(pythie)) {
 			System.out.println(pythie.getName() + " is older than one.");
-		}else {
+		} else {
 			System.out.println(pythie.getName() + " is younger than one.");
 		}
-		
-		if(isCatOlderThanOne.test(minou)) {
+
+		if (isCatOlderThanOne.test(minou)) {
 			System.out.println(minou.getName() + " is older than one.");
-		}else {
+		} else {
 			System.out.println(minou.getName() + " is younger than one.");
 		}
 	}
