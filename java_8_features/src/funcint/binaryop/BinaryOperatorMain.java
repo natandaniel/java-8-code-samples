a654e690058545eee9d2cd3bd3c0f70a0a276c70
Natan Daniel <natan.daniel0@gmail.com>
2020-01-12 23:34:39 +0100

    updating binary operator

--- java_8_features/src/funcint/binaryop/BinaryOperatorMain.java
+++ java_8_features/src/funcint/binaryop/BinaryOperatorMain.java
@@ -1,6 +1,7 @@
 package funcint.binaryop;
 
 import java.util.Comparator;
+import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
 
 /**
@@ -37,21 +38,31 @@ class BinaryOperatorMain {
 			return new TreasureBag(treasureBagOne.gold + treasureBagTwo.gold);
 		};
 
-		System.out.println("Adding treasure bags; expecting 30 gold coins; result : " + sumTreasureBags.apply(tenGoldCoins, twentyGoldCoins).gold + " gold coins");
+		System.out.println("Adding treasure bags; expecting 30 gold coins; result : "
+				+ sumTreasureBags.apply(tenGoldCoins, twentyGoldCoins).gold + " gold coins");
+
+		// BinaryOperator that sums TreasureBags then returns the result as a String.
+
+		BiFunction<TreasureBag, TreasureBag, String> sumTreasureBagsAndReturnResultAsString = sumTreasureBags
+				.andThen(treasureBag -> String.valueOf(treasureBag.gold));
+
+		System.out.println("Adding treasure bags and returning result as string; expecting 30 gold coins; result : "
+				+ sumTreasureBagsAndReturnResultAsString.apply(tenGoldCoins, twentyGoldCoins) + " gold coins");
 
 		// BinaryOperator that compares TreasureBags and returns the smallest bag
 
 		BinaryOperator<TreasureBag> smallestTreasureBag = BinaryOperator
 				.minBy((bag1, bag2) -> Integer.compare(((TreasureBag) bag1).gold, ((TreasureBag) bag2).gold));
-		
-		System.out.println("Comparing treasure bags; expecting bag with 10 gold coins; result : bag with " + smallestTreasureBag.apply(tenGoldCoins, twentyGoldCoins).gold + " gold coins");
 
-		
+		System.out.println("Comparing treasure bags; expecting bag with 10 gold coins; result : bag with "
+				+ smallestTreasureBag.apply(tenGoldCoins, twentyGoldCoins).gold + " gold coins");
+
 		// BinaryOperator that compares TreasureBags and returns the biggest bag
 
 		BinaryOperator<TreasureBag> biggestTreasureBag = BinaryOperator
 				.maxBy((bag1, bag2) -> Integer.compare(((TreasureBag) bag1).gold, ((TreasureBag) bag2).gold));
-		
-		System.out.println("Comparing treasure bags; expecting bag with 10 gold coins; result : bag with " + biggestTreasureBag.apply(tenGoldCoins, twentyGoldCoins).gold + " gold coins");		
+
+		System.out.println("Comparing treasure bags; expecting bag with 10 gold coins; result : bag with "
+				+ biggestTreasureBag.apply(tenGoldCoins, twentyGoldCoins).gold + " gold coins");
 	}
 }
